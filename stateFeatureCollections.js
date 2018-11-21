e691b307c41462e8f3361a25c2560d0fd2980ac8
Dave Ankin <alexanderankin@gmail.com>
2018-11-21 13:34:17 -0500

    added print statements

--- stateFeatureCollections.js
+++ stateFeatureCollections.js
@@ -22,21 +22,28 @@ console.log("Found", fiftys.length, "states to process.");
 
 
 function reduceState(state) {
+  console.log("Upper: Reading");
   var upperDir = pj(statesDir, state, 'sldu');
   var upperDs = fs.readdirSync(upperDir);
   var upperGeoJSON = upperDs.reduce(function (gJ, d) {
     var district = JSON.parse(fs.readFileSync(pj(upperDir, d), 'utf-8'));
     return gJ.features.push(district), gJ;
   }, { type: 'FeatureCollection', features: [] });
+  console.log("Upper: Writing");
   fs.writeFileSync(pj(upperDir, 'sldu.json'), JSON.stringify(upperGeoJSON));
   
+  console.log("Lower: Reading");
   var lowerDir = pj(statesDir, state, 'sldl');
   var lowerDs = fs.readdirSync(lowerDir);
   var lowerGeoJSON = lowerDs.reduce(function (gJ, d) {
     var district = JSON.parse(fs.readFileSync(pj(lowerDir, d), 'utf-8'));
     return gJ.features.push(district), gJ;
   }, { type: 'FeatureCollection', features: [] });
+  console.log("Lower: Writing");
   fs.writeFileSync(pj(lowerDir, 'sldl.json'), JSON.stringify(lowerGeoJSON));
 }
 
-fiftys.forEach(reduceState);
+fiftys.forEach(function (state) {
+  console.log("Processing state", state);
+  reduceState(state);
+});
